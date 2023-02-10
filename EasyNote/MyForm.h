d9cb0d6e79fe4e1fcb84818ee4f7cf131a485802
Pablo Alexander <pabloalexander.developer@gmail.com>
2023-02-11 00:00:21 +0100

    Implementacion de funciones a los botones y logica de entrada de username con password

--- EasyNote/MyForm.h
+++ EasyNote/MyForm.h
@@ -41,6 +41,8 @@ namespace EasyNote {
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Label^ label3;
 
 	private:
 		/// <summary>
@@ -62,6 +64,8 @@ namespace EasyNote {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -94,33 +98,66 @@ namespace EasyNote {
 			// 
 			this->textBox1->Location = System::Drawing::Point(157, 174);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(203, 26);
+			this->textBox1->Size = System::Drawing::Size(210, 26);
 			this->textBox1->TabIndex = 2;
 			// 
 			// textBox2
 			// 
 			this->textBox2->Location = System::Drawing::Point(157, 249);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(203, 26);
+			this->textBox2->Size = System::Drawing::Size(210, 26);
 			this->textBox2->TabIndex = 3;
+			this->textBox2->UseSystemPasswordChar = true;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(159, 326);
+			this->button1->Location = System::Drawing::Point(156, 357);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(91, 44);
 			this->button1->TabIndex = 4;
 			this->button1->Text = L"Sign in";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(269, 326);
+			this->button2->Location = System::Drawing::Point(269, 357);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(91, 44);
 			this->button2->TabIndex = 5;
 			this->button2->Text = L"Exit";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->BackColor = System::Drawing::Color::Transparent;
+			this->checkBox1->Checked = true;
+			this->checkBox1->CheckState = System::Windows::Forms::CheckState::Checked;
+			this->checkBox1->Font = (gcnew System::Drawing::Font(L"Arial", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->checkBox1->ForeColor = System::Drawing::Color::White;
+			this->checkBox1->Location = System::Drawing::Point(157, 295);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(102, 20);
+			this->checkBox1->TabIndex = 6;
+			this->checkBox1->Text = L"I agree to the";
+			this->checkBox1->UseVisualStyleBackColor = false;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Arial", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::Black;
+			this->label3->Location = System::Drawing::Point(251, 296);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(135, 16);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Terms and Condicions";
 			// 
 			// MyForm
 			// 
@@ -128,6 +165,8 @@ namespace EasyNote {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(502, 471);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->checkBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox2);
@@ -144,5 +183,39 @@ namespace EasyNote {
 
 		}
 #pragma endregion
+	private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) 
+	{
+		if (checkBox1->Checked) 
+		{
+			button1->Enabled = true;
+		}
+		else
+		{
+			button1->Enabled = false;
+		}
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		Application::Exit();
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		if (textBox1->Text == "admin")
+		{
+			if (textBox2->Text == "1234") 
+			{
+				Application::Exit();
+			}
+			else
+			{
+				MessageBox::Show("Incorrect Password","Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			}
+
+		}
+		else 
+		{	
+			MessageBox::Show("Incorrect Username", "Error", MessageBoxButtons::OK,MessageBoxIcon::Error);
+		}
+	}
 };
 }
